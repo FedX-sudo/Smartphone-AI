cb3161f44ea56e44e1f1e27d0eb4750f179d2dc6
FedX-sudo <66258975+FedX-sudo@users.noreply.github.com>
2021-04-14 15:04:02 -0600

    Delete smartphoneTHing-cyberReaach1 directory

--- smartphoneTHing-cyberReaach1/Motion sensor data preprocessing - Module/preprossesing.py
+++
@@ -1,3 +0,0 @@
-f = open("/home/fenixg/Code/cyberReachLabs/smartphoneTHing-cyberReaach1/Motion sensor data preprocessing - Module/CSM_MotionSensor_Dataset/CSM_MotionSensor_Dataset/User12Motion.txt", 'r');
-
-print (f);
